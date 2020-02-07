d62eb61a7c079f2d356a1ac122ac04b95bb01567
Chris Koehnke <chris.koehnke@elastic.co>
2020-02-07 11:28:36 -0500

    Use TLS 1.2 for chocolatey download
    Support for TLS 1.0 and 1.1 was removed from chocolatey on February 3rd
    2020 https://chocolatey.org/blog/remove-support-for-old-tls-versions.

--- scripts/chocolatey.bat
+++ scripts/chocolatey.bat
@@ -1,1 +1,4 @@
+# Use TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
